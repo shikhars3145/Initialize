7bbb4b6f7480b59ee402e76726a576782bf7004a
shikhars3145 <shikhars3145@gmail.com>
2021-04-27 00:41:31 +0530

    Contract files refactored and renamed

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,35 +1,6 @@
 // "SPDX-License-Identifier: UNLICENSED"
 pragma solidity ^0.8.0;
 
-contract CampaignFactory{
-    
-    Campaign[] deployedCampaigns;
-    
-    
-    function createCampaign(string memory _title, string memory _story, string memory _bannerImg, uint _minContribution, uint _goal) external {
-        Campaign newCampaign = new Campaign(_title, _story, _bannerImg, _minContribution, _goal);
-        deployedCampaigns.push(newCampaign);
-    }
-    
-    function getDeployedCampaignsAddr() view external returns(Campaign[] memory)
-    {
-        return deployedCampaigns;
-    }
-    
-    function getDeployedCampaignsDetails() view external returns(Campaign.Details[] memory)
-    {
-        Campaign.Details[] memory detailedCampaigns = new Campaign.Details[](deployedCampaigns.length);
-        
-        for(uint i = 0; i<deployedCampaigns.length; i++)
-        {
-            Campaign.Details memory temp = deployedCampaigns[i].getDetails();
-            detailedCampaigns[i] = temp;
-        }
-        
-        return detailedCampaigns;
-    }
-}
-
 contract Campaign{
     
     struct Request {
