784a9a7539fecb41fd31b7aa070cb634ffff113f
shikhars3145 <shikhars3145@gmail.com>
2021-04-27 21:10:07 +0530

    added transaction button to nav

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {AppBar, Toolbar, Typography} from '@material-ui/core';
+import TransactionButton from './TransactionButton';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -8,6 +9,10 @@ const useStyles = makeStyles((theme) => ({
     },
     title: {
       flexGrow: 1,
+    },
+    btn:{
+        width: '10rem',
+        color:'#fff'
     }
   }));
 
@@ -21,6 +26,7 @@ export default function Navbar() {
                 <Typography variant="h6" className={classes.title}>
                 INITIALIZE
                 </Typography>
+                <TransactionButton className={classes.btn} variant="outlined"/>
           </Toolbar>
         </AppBar>
       </div>
