e257bfce3708b26da93d9e4ca15227d7ac647953
shikhars3145 <shikhars3145@gmail.com>
2021-04-27 08:49:05 +0530

    added campaign deployed event

--- contracts/CampaignFactory.sol
+++ contracts/CampaignFactory.sol
@@ -5,11 +5,12 @@ import "./Campaign.sol";
 contract CampaignFactory{
     
     Campaign[] deployedCampaigns;
-    
+    event campaignDeployed(address contractAddr);
     
     function createCampaign(string memory _title, string memory _story, string memory _bannerImg, uint _minContribution, uint _goal) external {
         Campaign newCampaign = new Campaign(_title, _story, _bannerImg, _minContribution, _goal);
         deployedCampaigns.push(newCampaign);
+        emit campaignDeployed(address(newCampaign));
     }
     
     function getDeployedCampaignsAddr() view external returns(Campaign[] memory)
