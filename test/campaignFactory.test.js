a990c21ef72c5e50a758e8dc72125f5ea2545547
shikhars3145 <shikhars3145@gmail.com>
2021-04-27 09:23:28 +0530

    campaign factory test added

---
+++ test/campaignFactory.test.js
@@ -0,0 +1,25 @@
+const CampaignFactory = artifacts.require('./CampaignFactory');
+
+contract('factory',async()=>{
+
+    let factory;
+
+    beforeEach(async()=>{
+        factory = await CampaignFactory.deployed();
+        console.log("before each called")
+    })
+
+    it('deployed',async()=>{
+        
+        console.log(factory.address);
+        assert.ok(factory.address);
+    })
+
+    it('deploys campaign', async()=>{
+        const receipt = await factory.createCampaign("Interesting title","convincing story","ipfs image path",100,5000);
+        // console.log(receipt);
+        const deployedCampaigns = await factory.getDeployedCampaignsAddr();
+        // console.log(deployedCampaigns[0]);
+        assert.ok(deployedCampaigns[0]);
+    })
+})
