f7dd4a3c7f1495df925584f68dfefae9dbdcc701
shikhars3145 <shikhars3145@gmail.com>
2021-04-27 21:09:47 +0530

    Created Transaction Button component

---
+++ client/src/components/TransactionButton.js
@@ -0,0 +1,44 @@
+import React, { useContext } from 'react'
+import Button from '@material-ui/core/Button';
+import UserContext from '../contexts/user/user.context';
+
+
+export default function TransactionButton({children, onClick, type,...btnProps}) {
+    const {user,setUser} = useContext(UserContext);
+
+    if(typeof ethereum !== 'undefined')
+    {
+        ethereum.on('accountsChanged',(accounts)=>{
+            setUser(accounts[0]);
+        })
+
+        ethereum.on('chainChanged', (_chainId) => window.location.reload());
+    }
+
+    const connectWallet =()=> {
+        if(typeof ethereum !== 'undefined'){
+            ethereum.request({ method: 'eth_requestAccounts' }).then(accounts=>{
+                setUser(accounts[0]);
+            }).catch(err=>{console.log(err);alert(err.message);})
+        }
+        else{
+            alert("No Wallet Detected Please Consider Using Metamask")
+        }
+    }
+
+
+
+    return (<Button onClick={user?onClick:connectWallet} type={user?type:"button"}{...btnProps}>
+            {
+                user?
+                    (children?
+                        children
+                        :
+                        `${user.slice(0,6)}...${user.slice(-6)}`)
+                    :
+                    "Connect Wallet"
+            }
+        </Button>
+    )
+    
+}
