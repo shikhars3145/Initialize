84c13e12b6ce08449a9cc55fd35e0b2be38e2b66
shikhars3145 <shikhars3145@gmail.com>
2021-04-28 11:13:36 +0530

    created campaign factory contract instance

---
+++ client/src/utils/factory.js
@@ -0,0 +1,23 @@
+const web3 = require('./web3');
+const CampaignFactory = require('../abi/CampaignFactory.json');
+
+// const factory = new web3.eth.Contract(CampaignFactory.abi,"0x1BD6A15a4ceFaFDcbD893E42c3B8878d64723642");
+// const networkId = await web3.eth.net.getId();
+
+// const networkId = "5777"; // "Development/ganache"
+// const networkId = "3"; // "Ropsten"
+const networkId = process.env.DEPLOYED_NETWORK;
+console.log(networkId);
+// console.log("networkData",CampaignFactory.networks[networkId]);
+const networkData = CampaignFactory.networks[networkId];
+if(networkData){
+    factory = new web3.eth.Contract(CampaignFactory.abi,networkData.address);
+}
+else{
+    if(typeof alert !== 'undefined')
+        alert("Contract not deployed to current network");
+    console.log("Contract not deployed to current network");
+    throw("Contract not deployed to current network");
+}
+// console.log("factory",factory);
+module.exports = factory;
