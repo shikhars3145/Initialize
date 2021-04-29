65571cdd7081efca49c57c311c44068048b09f70
shikhars3145 <shikhars3145@gmail.com>
2021-04-30 01:05:09 +0530

    handled undefined props

--- client/src/components/CampaignShowcase.js
+++ client/src/components/CampaignShowcase.js
@@ -29,15 +29,16 @@ const useStyles = makeStyles((theme) => ({
   }));
 
 const sortFunc = (sortBy,filtered) =>{
-    if(sortBy==="oldest") return filtered;
     if(sortBy==="latest") return filtered.reverse();
     if(sortBy==="funds") return filtered.sort((a,b)=>a[4]-b[4]);
     if(sortBy==="percent") return filtered.sort((a,b)=>(b[4]/b[3])-(a[4]/a[3]));
     if(sortBy==="alpha") return filtered.sort((a,b)=>(a[0].toLowerCase()<b[0].toLowerCase()?-1:1));
+    return filtered;
 }
 
 export default function CampaignsShowcase({campaigns, limit, sortBy, searchQuery}) {
     const classes = useStyles();
+    if(!searchQuery) searchQuery="";
     const filtered = campaigns.filter(campaign => campaign[0].toLowerCase().includes(searchQuery.toLowerCase()));
     const sorted = sortFunc(sortBy,filtered);
     const limited = sorted.slice(0,limit);
