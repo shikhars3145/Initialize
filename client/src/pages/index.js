254b74f479c9a2ea8c4cb095c93169505235ed26
shikhars3145 <shikhars3145@gmail.com>
2021-05-04 03:34:13 +0530

    removed dummy data and fetched all campaigns' details from factory smart contract

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,26 +1,25 @@
 import { Container, Typography } from '@material-ui/core';
 import CampaignsShowcase from '../components/CampaignShowcase';
 import Hero from '../components/Hero'
+import factory from '../utils/factory'
 
-export default function Home() {
+export default function Home({campaigns}) {
 
-  const camps = [
-    ['Release The Snyder Cut', 'Some story', 'brokeimage', 500, 700, 4000000000000, '0x843468468684'],
-    ['Some Interesting name', 'Some story', 'brokeimage', 1800, 700, 5278000000, '0x843468468684'],
-    ['Release The Snyder Verse', 'Some story', 'brokeimage', 500, 1800, 40000000000010, '0x843468468684'],
-    ['Boring Company', 'Some story', 'brokeimage', 5100, 7200, 40000000000010, '0x843468468684'],
-    ['Dunkin Donut', 'Some story', 'brokeimage', 900, 500, 40000000000010, '0x843468468684'],
-    ['Nolan is genius', 'Some story', 'brokeimage', 500, 1200, 40000000000010, '0x843468468684'],
-    ['Oxygen Help', 'Some story', 'brokeimage', 100, 700, 40000000000010, '0x843468468684'],
-    ['Need Medicine', 'Some story', 'brokeimage', 700, 700, 40000000000010, '0x843468468684'],
-  ];
   return (
       <Container style={{textAlign:"center"}}>
       <Hero/>
       <Typography variant="h4" style={{marginBottom:"2rem"}}>
         Featured Campaigns:
       </Typography>
-      <CampaignsShowcase campaigns={camps} searchQuery="" sortBy="percent" limit={3}/>
+      <CampaignsShowcase campaigns={campaigns} searchQuery="" sortBy="percent" limit={3}/>
       </Container>
   )
+}
+
+export async function getServerSideProps(context) {
+  if(!factory) return {props:{campaigns:[]}}
+  const campaigns = await factory.methods.getDeployedCampaignsDetails().call();
+  return {
+    props: {campaigns}, // will be passed to the page component as props
+  }
 }
