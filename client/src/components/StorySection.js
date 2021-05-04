dcc61632742500661725437ac2ca2b13f4dc4bf2
shikhars3145 <shikhars3145@gmail.com>
2021-05-05 05:17:46 +0530

    added styling to story section

--- client/src/components/StorySection.js
+++ client/src/components/StorySection.js
@@ -1,16 +1,26 @@
-import { Typography } from '@material-ui/core'
+import { makeStyles, Typography } from '@material-ui/core'
 import React from 'react'
+const useStyles = makeStyles((theme)=>({
+    heading:{
+        margin: "1rem auto"
+    },
+    story:{
+        whiteSpace: "pre-line"
+    }
+}))
 
 export default function StorySection({story}) {
+    console.log("story rerendered");
+    const classes = useStyles();
     return (
         <>
-        <Typography variant="h5">
+        <Typography variant="h4" color="primary" className={classes.heading}>
             Story
         </Typography>
-        <Typography variant="body1" align="justify">
-        <pre>
+        <Typography className={classes.story} variant="body1" align="justify">
+
             {story}
-        </pre>
+
         </Typography>
         </>
     )
