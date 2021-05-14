8279a6943be2dc3ec23310d40a17c8735fdce7ea
shikhars3145 <shikhars3145@gmail.com>
2021-05-14 09:24:24 +0530

    Added Jazzicon to txnbtn

--- client/src/components/TransactionButton.js
+++ client/src/components/TransactionButton.js
@@ -1,36 +1,57 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import Button from '@material-ui/core/Button';
 import UserContext from '../contexts/user/user.context';
 import web3 from '../utils/web3';
+import { makeStyles } from '@material-ui/core';
+import Jazzicon, { jsNumberForAddress } from 'react-jazzicon';
 
+const useStyles = makeStyles(theme => ({
+    icon:{ 
+        padding: 2,
+        border: `5px solid ${theme.palette.secondary.main}`,
+        height: 46,
+        width:46,
+        borderRadius:"50%",
+        marginRight:5
+      },
+}));
 
-export default function TransactionButton({children, onClick, type,...btnProps}) {
-    const {user,setUser} = useContext(UserContext);
+export default function TransactionButton({ children, onClick, type, ...btnProps }) {
+  const { user, setUser } = useContext(UserContext);
+  const classes = useStyles();
 
-    const connectWallet =()=> {
-        if(typeof ethereum !== 'undefined'){
-            ethereum.request({ method: 'eth_requestAccounts' }).then(accounts=>{
-                setUser(web3.utils.toChecksumAddress(accounts[0]));
-            }).catch(err=>{console.log(err);alert(err.message);})
-        }
-        else{
-            alert("No Wallet Detected Please Consider Using Metamask")
-        }
+  const connectWallet = () => {
+    if (typeof ethereum !== 'undefined') {
+      ethereum
+        .request({ method: 'eth_requestAccounts' })
+        .then(accounts => {
+          setUser(web3.utils.toChecksumAddress(accounts[0]));
+        })
+        .catch(err => {
+          console.log(err);
+          alert(err.message);
+        });
+    } else {
+      alert('No Wallet Detected Please Consider Using Metamask');
     }
+  };
 
-
-
-    return (<Button onClick={user?onClick:connectWallet} type={user?type:"button"}{...btnProps}>
-            {
-                user?
-                    (children?
-                        children
-                        :
-                        `${user.slice(0,6)}...${user.slice(-6)}`)
-                    :
-                    "Connect Wallet"
-            }
-        </Button>
-    )
-    
+  return (
+    <Button style={{width:"fit-content"}} onClick={user ? onClick : connectWallet} type={user ? type : 'button'} {...btnProps}>
+      {user ? (
+        children ? (
+          children
+        ) : (
+          <>
+            <span class={classes.icon}>
+              <Jazzicon diameter={32} seed={jsNumberForAddress(user)} />
+            </span>
+            {`  ${user.slice(0, 6)}...${user.slice(-6)}`}
+          </>
+        )
+      ) : (
+        'Connect Wallet'
+      )}
+    </Button>
+  );
 }
