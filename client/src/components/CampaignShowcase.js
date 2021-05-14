1e9d4654b2df46080fd51b19d654e2cc103ae5af
shikhars3145 <shikhars3145@gmail.com>
2021-05-14 09:26:27 +0530

    added sm breakpoint to grid

--- client/src/components/CampaignShowcase.js
+++ client/src/components/CampaignShowcase.js
@@ -49,7 +49,7 @@ export default function CampaignsShowcase({campaigns, limit, sortBy, searchQuery
         <Grid container spacing={3} >
             {
                 limited.map((campaign, idx) => (
-                    <Grid item xs={12} sm={3} key={idx} className={classes.gridCell}>
+                    <Grid item xs={12} sm={6} md={3} key={idx} className={classes.gridCell}>
                             <CampaignCard campaign={campaign} />
                     </Grid>
                 ))
