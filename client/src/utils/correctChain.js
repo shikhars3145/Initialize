d03bd77ff4d7d13195e9ff4ccd9abbdaa830088f
shikhars3145 <shikhars3145@gmail.com>
2021-05-15 09:00:22 +0530

    added utility function for checking if chain is correct or not

---
+++ client/src/utils/correctChain.js
@@ -0,0 +1,23 @@
+import web3 from '../utils/web3';
+
+const correctChain = async()=> {
+    const networkId = await web3.eth.net.getId();
+    const DEPLOYED_NETWORK_ID = process.env.DEPLOYED_NETWORK_ID;
+
+    if (DEPLOYED_NETWORK_ID != networkId)
+    {
+        if(typeof alert !== 'undefined')
+        alert(`Please Change network to ${chainMap[DEPLOYED_NETWORK_ID]} network`);
+        return false;
+    } 
+    else return true;
+}
+
+export const chainMap = {
+    5777: "Ganache",
+    1: "Ethereum Mainnet",
+    3: "Ropsten",
+    4: "Rinkeby" 
+}
+export default correctChain;
+
